59df5d10ad57b37370cfa9f4345275cd3ab0eed3
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-11-11 22:54:43 +0100

    initializing project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+import { Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import Router from './Router';
+import { store } from '../../redux/store';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#AB3F21',
+    },
+    secondary: {
+      main: '#2A2B2E',
+    },
+    neutral: {
+      main: '#646464',
+      contrastText: '#fff',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: 'bold',
+        },
+      },
+    },
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <Container>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Container>
+  </Provider>
+);
+
+export default App;
