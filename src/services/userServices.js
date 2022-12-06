ed503be88aad28c6b72324f771f5360d924f7cd5
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-12-06 17:56:25 +0100

    adding search for participants axios

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -8,6 +8,10 @@ export const getUser = (body) => instance.post('/user/user', body)
   .then((response) => response)
   .catch((error) => error);
 
+export const searchUser = (body) => instance.post('/user/search', body)
+  .then((response) => response)
+  .catch((error) => error);
+
 export const updateUser = (userId, body) => instance.put(`/user/update/${userId}`, body)
   .then((response) => response)
   .catch((error) => error);
